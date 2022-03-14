d04a4677f958a819e3343d4670ee322269ad8a27
Eka Gharibashvili <ekagaribashvili98@gmail.com>
2022-03-14 21:27:09 +0400

    feat: jwt

--- index.js
+++ index.js
@@ -1,11 +1,47 @@
 const express = require("express");
-
+const jwt = require("jsonwebtoken");
 const app = express();
 const port = 3000;
 
 // parse json using express
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.get("/api", (req, res) => {
+  res.json({ message: "Hey there welcome to this API service" });
+});
+
+app.post("/api/posts", verifyToken, (req, res) => {
+  res.json({ message: "Posts created..." });
+});
+
+app.post("/api/login", (req, res) => {
+  const user = {
+    id: 1,
+    username: "John",
+    email: "john@gmail.com",
+  };
+  jwt.sign({ user: user }, "secretkey", (err, token) => {
+    res.json({
+      token,
+    });
+  });
+});
+
+function verifyToken(req, res, next) {
+  const bearerHeader = req.headers["authorization"];
+
+  // console.log(req.headers);
+  if (typeof bearerHeader !== "undefined") {
+    //   console.log(bearerHeader)
+    const bearerToken = bearerHeader.split(" ")[1];
+    req.token = bearerToken;
+    next();
+  } else {
+    res.sendStatus(401);
+  }
+}
 
 let movies = [
   {
@@ -49,11 +85,11 @@ app.get("/movie/:id", (req, res) => {
 });
 
 // remove movie from the list
-app.delete('/movie/:id', (req, res) => {
-    const id = req.params.id;
-    movies= movies.filter(movie => movie.id !== id)
-    res.status(200).send(movies)
-})
+app.delete("/movie/:id", (req, res) => {
+  const id = req.params.id;
+  movies = movies.filter((movie) => movie.id !== id);
+  res.status(200).send(movies);
+});
 
 // set the server to listen
 app.listen(port, () => console.log(`server is listening at port ${port}`));
