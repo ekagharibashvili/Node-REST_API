1dfcd71d2bbb1da9bab873a07f6f52ee16292d4d
Eka Gharibashvili <ekagaribashvili98@gmail.com>
2022-03-14 17:48:29 +0400

    feat: starter

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+const port = 3000
+
+let movies = [{
+    id: '1',
+    title: 'inception',
+    director: 'Christopher Nolan',
+    release_date: '2010-07-16'
+}]
